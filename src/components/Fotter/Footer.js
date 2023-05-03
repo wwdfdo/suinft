1b115c26559e1c4b315c23b784390c8a8878fa51
wwdfdo <wwdfdo@gmail.com>
2023-05-03 21:16:28 +0530

    added new styles to footer

--- src/components/Fotter/Footer.js
+++ src/components/Fotter/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import sound from "../../audios/audio1.mp3";
+// import sound from "../../audios/audio1.mp3";
 
 const Footer = () => {
   // const audioRef = useRef(null);
@@ -12,14 +12,14 @@ const Footer = () => {
   return (
     <div className="text-center bg-[#3A94C9] h-16 flex items-center justify-center  font-semibold shadow-3xl relative">
       <p>© 2023 by Sui 水. All rights reserved!</p>
-      <audio
+      {/* <audio
         // ref={audioRef}
         controls
         autoPlay={true}
         loop={true}
         src={sound}
         className="fixed bottom-0 right-0 w-[230px] text-sm hidden lg:block"
-      ></audio>
+      ></audio> */}
     </div>
   );
 };
